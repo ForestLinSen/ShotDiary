2aff3608f29b92aa42e8ed57d26faae15eb7a686
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-05-20 00:22:29 +0800

    full-text mode

--- ShotDiary/Controllers/Others/Diary/DiaryCellViewController.swift
+++ ShotDiary/Controllers/Others/Diary/DiaryCellViewController.swift
@@ -15,6 +15,7 @@ class DiaryCellViewController: UIViewController {
     var playerLayer: AVPlayerLayer?
     weak var delegate: WritingDiaryViewControllerDelegate?
     private var observer: NSObjectProtocol?
+    private var fullTextMode = false
     
     private let titleLabel: UILabel = {
         let label = UILabel()
@@ -28,8 +29,41 @@ class DiaryCellViewController: UIViewController {
         label.font = .systemFont(ofSize: 16, weight: .regular)
         label.numberOfLines = 0
         label.textColor = .white
+        label.clipsToBounds = true
         return label
-        
+    }()
+    
+    private let fullTextView: UITextView = {
+        let textView = UITextView()
+        textView.allowsEditingTextAttributes = false
+        textView.textColor = .white
+        textView.font = .systemFont(ofSize: 16)
+        textView.backgroundColor = .clear
+        return textView
+    }()
+    
+    private let showFullTextButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = K.mainNavy
+        button.setTitle("Full Text", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    private let videoModeButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = K.mainNavy
+        button.setTitle("Video Mode", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = K.mainNavyBackground
+        return view
     }()
 
     override func viewDidLoad() {
@@ -40,6 +74,7 @@ class DiaryCellViewController: UIViewController {
         configure()
         tabBarController?.tabBar.isHidden = true
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis"), style: .plain, target: self, action: #selector(didTapActionButton))
+        showFullTextButton.addTarget(self, action: #selector(didTapFullTextButton), for: .touchUpInside)
     }
     
     @objc func didTapActionButton(){
@@ -80,18 +115,88 @@ class DiaryCellViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        let padding = CGFloat(20)
-        let contentWidth = CGFloat(300)
-        let titleLabelHeight = CGFloat(50)
-        let contentLabelHeight = CGFloat(150)
         
-        titleLabel.frame = CGRect(x: padding, y: view.frame.height-titleLabelHeight-contentLabelHeight-padding*2, width: contentWidth, height: titleLabelHeight)
-        contentLabel.frame = CGRect(x: padding, y: view.frame.height-contentLabelHeight-padding*2, width: contentWidth, height: contentLabelHeight)
+        if !fullTextMode{
+            let padding = CGFloat(20)
+            let contentWidth = CGFloat(300)
+            let titleLabelHeight = CGFloat(65)
+            let contentLabelHeight = CGFloat(150)
+            
+            titleLabel.frame = CGRect(x: padding, y: view.frame.height-titleLabelHeight*2-contentLabelHeight-padding*2, width: contentWidth, height: titleLabelHeight)
+            contentLabel.frame = CGRect(x: padding, y: view.frame.height-titleLabelHeight-contentLabelHeight-padding*2, width: contentWidth, height: contentLabelHeight)
+            
+            titleLabel.sizeToFit()
+            contentLabel.sizeToFit()
+            
+            contentLabel.frame.size.height = min(contentLabel.frame.height, contentLabelHeight)
+            showFullTextButton.frame = CGRect(x: contentLabel.frame.origin.x,
+                                              y: contentLabel.frame.origin.y + contentLabel.frame.height + padding,
+                                              width: 125,
+                                              height: 45)
+        }
         
-        titleLabel.sizeToFit()
-        contentLabel.sizeToFit()
         
-        contentLabel.frame.size.height = min(contentLabel.frame.height, contentLabelHeight)
+        
+    }
+    
+    @objc func didTapFullTextButton(){
+        view.addSubview(backgroundView)
+        view.addSubview(videoModeButton)
+        view.addSubview(fullTextView)
+        backgroundView.frame = CGRect(x: 0, y: view.frame.height, width: view.frame.width, height: view.frame.height)
+        fullTextView.frame = CGRect(x: 20, y: view.frame.height-65-150-40, width: 300, height: 150)
+        
+        UIView.animate(withDuration: 0.25) {
+            self.showFullTextButton.removeFromSuperview()
+            self.backgroundView.frame = self.view.bounds
+            
+            self.view.bringSubviewToFront(self.titleLabel)
+            
+            let padding = CGFloat(20)
+            let contentWidth = self.view.frame.width*0.9
+            let titleLabelHeight = CGFloat(65)
+            let contentLabelHeight = self.view.frame.height-padding*2-titleLabelHeight-185
+            let bottom = self.navigationController?.navigationBar.frame.size.height ?? 0
+            
+            self.titleLabel.frame = CGRect(x: padding, y: bottom+padding*4, width: contentWidth, height: titleLabelHeight)
+            self.fullTextView.frame = CGRect(x: padding, y: bottom+titleLabelHeight+padding*4, width: contentWidth, height: contentLabelHeight)
+            
+            self.fullTextView.text = self.contentLabel.text
+            
+            self.titleLabel.sizeToFit()
+            self.fullTextView.sizeToFit()
+            self.fullTextView.frame.size.height = min(self.fullTextView.frame.height, contentLabelHeight)
+            
+            self.contentLabel.removeFromSuperview()
+            
+            self.videoModeButton.frame = CGRect(x: self.fullTextView.frame.origin.x,
+                                                y: self.fullTextView.frame.origin.y + self.fullTextView.frame.height + padding,
+                                                width: 125,
+                                                height: 45)
+            
+            self.fullTextMode = true
+            self.viewDidLayoutSubviews()
+            
+        }
+        
+        videoModeButton.addTarget(self, action: #selector(didTapVideoModeButton), for: .touchUpInside)
+        
+    }
+    
+    @objc func didTapVideoModeButton(){
+        
+        videoModeButton.removeFromSuperview()
+        fullTextView.removeFromSuperview()
+        view.addSubview(contentLabel)
+        view.addSubview(showFullTextButton)
+        contentLabel.frame = fullTextView.frame
+        
+        self.fullTextMode = false
+        UIView.animate(withDuration: 0.25) {
+            self.backgroundView.removeFromSuperview()
+            self.viewDidLayoutSubviews()
+        }
+        
     }
     
     init(viewModel: DiaryViewModel){
@@ -132,6 +237,7 @@ class DiaryCellViewController: UIViewController {
     private func configure(){
         view.addSubview(titleLabel)
         view.addSubview(contentLabel)
+        view.addSubview(showFullTextButton)
         titleLabel.text = viewModel.title
         contentLabel.text = viewModel.content
     }
